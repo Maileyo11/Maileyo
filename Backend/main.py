5d9d2105974aa9fdd5f61a8ae5a80beb93408bbd
Scorpion <164779656+scorpion-00@users.noreply.github.com>
2025-08-31 00:52:33 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -14,17 +14,17 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "https://maileyo.in",       
-    "https://www.maileyo.in",   
-    "http://localhost:8000",    
-    "http://localhost:8080",    
-    "http://localhost:5173",    
-]
+# origins = [
+#     "https://maileyo.in",       
+#     "https://www.maileyo.in",   
+#     "http://localhost:8000",    
+#     "http://localhost:8080",    
+#     "http://localhost:5173",    
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
